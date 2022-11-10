1a540428215b2430cb94d550feee4b77aec85142
Vitaliy Gerassimov <102645214+VitaliyDeveloper@users.noreply.github.com>
2022-11-10 10:28:11 +0600

    fix bag

--- app.js
+++ app.js
@@ -15,29 +15,26 @@ for (let i = 0; i < SQUARES_NUMBER; i += 1) {
   const square = document.createElement("div");
   square.classList.add("square");
 
-  square.addEventListener("mouseover", () => {
-    setColor(square);
-  });
+  square.addEventListener("mouseover", setColor);
 
-  square.addEventListener("mouseleave", () => {
-    removeColor(square);
-  });
+  square.addEventListener("mouseleave", removeColor);
 
   board.append(square);
 }
 
-function setColor(el) {
+function setColor(event) {
+  const el = event.target;
   const color = getRamdomColor();
   el.style.backgroundColor = color;
   el.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
-function removeColor(el) {
+function removeColor(event) {
+  const el = event.target;
   el.style.boxShadow = `0 0 2px #000`;
   el.style.backgroundColor = "#1d1d1d";
 }
 
 function getRamdomColor() {
-  const index = Math.floor(Math.random() * colors.length);
-  return colors[index];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
